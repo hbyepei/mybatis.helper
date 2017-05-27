6172623030f3aa1052d6ef295278bf96124ba32f
yepei <yepei@meituan.com>
2017-05-27 17:00:19 +0800

    转成插件

--- src/yp/dev/tools/Main.java
+++ src/yp/dev/tools/Main.java
@@ -4,6 +4,8 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Author:yepei@meituan.com
  * Date:2017/5/27
@@ -15,18 +17,28 @@ public class Main extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
+        final ClassLoader mainLoader = Thread.currentThread().getContextClassLoader();
         final String[] args = new String[]{};
-        final ClassLoader loader = JavaFxBootStrap.class.getClassLoader();
+        final ClassLoader subLoader = JavaFxBootStrap.class.getClassLoader();
+        final CountDownLatch latch = new CountDownLatch(1);
         new Thread() {
             @Override
             public void run() {
                 try {
-                    Thread.currentThread().setContextClassLoader(loader);
+                    Thread.currentThread().setContextClassLoader(subLoader);
                     JavaFxBootStrap.main(args);
                 } catch (Exception e) {
                     e.printStackTrace();
+                } finally {
+                    latch.countDown();
+                    Thread.currentThread().setContextClassLoader(mainLoader);
                 }
             }
         }.start();
+        try {
+            latch.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
